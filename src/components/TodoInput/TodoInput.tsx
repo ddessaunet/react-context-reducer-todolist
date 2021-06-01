14771c8ab0ae886fd32ae486961738904af5f7b8
Darío <ddessaunet@gmail.com>
2021-05-31 21:02:20 -0300

    Add todo markup

--- src/components/TodoInput/TodoInput.tsx
+++ src/components/TodoInput/TodoInput.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-import style from "./TodoInput";
+import "./TodoInput.css";
 
 type TodoInputProps = {
   children?: React.ReactNode;
 };
 export const TodoInput = ({ children }: TodoInputProps): JSX.Element => {
   return (
-    <div>
-      <h1>TodoInput component</h1>
+    <div className="TodoInput_container">
+      <h1>useCONTEXT + useREDUCER TO-DO LIST</h1>
+      <input className="TodoInput_input" type="text" />
+      <button className="TodoInput_button" onClick={() => {}}>
+        ADD
+      </button>
     </div>
   );
 };
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 
-import style from "./TodoList";
+import "./TodoList.css";
 
 type TodoListProps = {
   children?: React.ReactNode;
 };
 export const TodoList = ({ children }: TodoListProps): JSX.Element => {
   return (
-    <div>
-      <h1>TodoList component</h1>
+    <div className="TodoList_container">
+      <h1>TO-DOS</h1>
+      <ul className="TodoList_list">
+        <li className="TodoList_element">
+          <span>TODO TEXT</span>
+          <div className="TodoList_buttons">
+            <button onClick={() => {}}>TOGGLE</button>
+            <button onClick={() => {}}>DELETE</button>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 };
